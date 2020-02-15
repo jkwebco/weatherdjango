495df9c939127602f1baa7a354beb3ae54d4dfe1
jkwebco <jkwebdesignco@gmail.com>
2020-02-15 17:42:33 -0500

    Initial commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/userany/Downloads/django/weather_app_django/the_weather/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
