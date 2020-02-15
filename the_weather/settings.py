692637233c322acb1e8ca2a37ea181252692672d
jkwebcoAI <jkwebdesignco@gmail.com>
2020-02-15 18:50:32 -0500

    Update settings.py

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'the_weather.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
